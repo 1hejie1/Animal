62657f719e5cc36dd0c70934299ee8a6e1c528ad
hejie <1hejie1@gmail.com>
2018-12-03 09:13:12 +0800

    version 0.16.6 commit

---
+++ animal/animal.h
@@ -0,0 +1,44 @@
+/*
+ * -------------------------------------------------------------------------
+ * ANIMAL - ANIMAL IMage Processing LibrarY
+ * Copyright (C) 2002,2003-2011  Ricardo Fabbri
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * -------------------------------------------------------------------------
+ */ 
+  
+
+#ifndef ANIMAL_H 
+#define ANIMAL_H 1
+
+#include "common.h"
+#include "hello.h"
+#include "util.h"
+#include "img.h"
+#include "plist.h"
+#include "pstk.h"
+#include "queue.h"
+#include "set.h"
+#include "array.h"
+#include "sedr.h"
+#include "adjacency.h"
+#include "analysis.h"
+#include "filtering.h"
+#include "segment.h"
+#include "morphology.h"
+#include "ift.h"
+#include "distmaps.h"
+
+#endif /* !ANIMAL_H */
